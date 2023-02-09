ad2677c2354ef7fab1cc62f29baf4dbbfd025ec4
Paucinha <paucia.renan@gmail.com>
2023-02-09 14:47:48 -0300

    adicionado arquivos e pastas

---
+++ src/main/java/me/dio/academia/digital/entity/form/MatriculaForm.java
@@ -0,0 +1,18 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MatriculaForm {
+
+    @NotNull(message = "Preencha o campo corretamente.")
+    @Positive(message = "O Id do aluno precisa ser positivo.")
+    private Long alunoId;
+}
